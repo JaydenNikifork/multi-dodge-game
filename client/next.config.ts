6d22dca2dd1ca9b902a4bda726f23da0e552a25e
Jayden Nikifork <jnikifor@uwaterloo.ca>
2026-01-10 19:27:59 -0500

    change root path

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  basePath: "/multi-dodge-game",
+  assetPrefix: "/multi-dodge-game",
 };
 
 export default nextConfig;
