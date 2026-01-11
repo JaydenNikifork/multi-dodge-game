07c9c908f2f9f82c17317316f028f06c413e4236
Jayden Nikifork <jnikifor@uwaterloo.ca>
2026-01-10 19:13:31 -0500

    get build working?

--- client/utils/consts.ts
+++ client/utils/consts.ts
@@ -2,7 +2,9 @@ import { Vec2 } from "shared";
 
 export const URL = "ws://localhost:8080";
 export const SCREEN_SIZE: Vec2 = {
-  x: window.innerWidth,
-  y: window.innerHeight,
+  // x: window.innerWidth,
+  // y: window.innerHeight,
+  x: 1700,
+  y: 800
 }
 
